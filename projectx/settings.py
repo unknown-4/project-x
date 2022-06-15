435aa8b1505a38cbc034537bc074ba6ca08d05a3
Deo saju <Deosaju2260@gmail.com>
2022-06-15 16:02:06 +0530

    Langind page

--- projectx/settings.py
+++ projectx/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7_dvglzwzt@s_hm%nc=315!3vp0+u+dpec2de&_$k%o)2-d=jl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
