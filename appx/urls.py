cb45a0f95ad62ac7f7dd204a12fef8dabfcc076f
Deo saju <Deosaju2260@gmail.com>
2022-06-17 15:20:59 +0530

    login

--- appx/urls.py
+++ appx/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path("login_user", views.login_user, name="login_user"),
+    path("logout_user", views.logout_user, name="logout_user"),
 ]
--- appx/views.py
+++ appx/views.py
@@ -1,5 +1,59 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Invalid Username or Password')
+            return redirect('login_user')
+    else:
+        return render(request, 'login.html')
+
+def logout_user(request):
+    auth.logout(request)
+    return redirect('index')
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password==confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is already taken')
+                return redirect(register)
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email is already taken')
+                return redirect(register)
+            else:
+                user = User.objects.create_user(username=username, password=password, 
+                                        email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                
+                return redirect('login_user')
+
+
+        else:
+            messages.info(request, 'Both passwords are not matching')
+            return redirect(register)
+            
+
+    else:
+        return render(request, 'registeration.html')
